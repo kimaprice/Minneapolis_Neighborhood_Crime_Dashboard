cd8ae072ec549b8a06af048fc9ef3ebc62a98635
Kim Price <k_renick@yahoo.com>
2022-10-06 19:27:44 -0500

    minor directory changes

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 import os
 import psycopg2
+from sqlalchemy import create_engine, inspect
 import get_data
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Connect to postgreSQL DB
+protocol = 'postgresql'
+username = 'postgres'
+password = 'bootcamp'
+host = 'localhost'
+port = 5432
+database_name = 'minne_crime_db'
+connection_string = f'{protocol}://{username}:{password}@{host}:{port}/{database_name}'
+engine = create_engine(connection_string)
+
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='minne_crime_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return conn
+
 
 
 # Route to render index.html template using data from Mongo
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT statement')
-    books = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', books=books)
+   
+
 
 
 # Route to gather neighborhood specific data
