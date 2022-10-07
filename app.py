58848a167db813d6d1037f155bfc6dec91124b01
Kim Price <k_renick@yahoo.com>
2022-10-07 14:08:54 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from flask import Flask, render_template, redirect
-import os
-import psycopg2
-import get_data
+from flask import Flask, render_template, redirect, jsonify
+
+
+#Import json
+import json
+
+#Import from SQL Alchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+# Define the PostgreSQL connection parameters
+username = 'postgres'  # Ideally this would come from config.py (or similar)
+password = 'bootcamp'  # Ideally this would come from config.py (or similar)
+database_name = 'minne_crime_db' 
+port_number = '5432' # Check your own port number! It's probably 5432, but it might be different!
+connection_string = f'postgresql://{username}:{password}@localhost:{port_number}/{database_name}'
+
+# Connect to the SQL database
+engine = create_engine(connection_string)
+base = automap_base()
+base.prepare(engine, reflect=True)
+
+#Reflect the tables
+crime_data = base.classes.crime_data
+demo_cat = base.classes.demo_cat
+demographic_data = base.classes.demographic_data
+neighborhood = base.classes.neighborhood
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Connect to postgreSQL DB
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='minne_crime_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return conn
-
-
 # Route to render index.html template using data from Mongo
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT statement')
-    books = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', books=books)
+
+    return render_template('index.html')
 
 
 # Route to gather neighborhood specific data
@@ -34,6 +44,27 @@ def getData():
     # Redirect back to home page
     return redirect("/")
 
+@app.route("/readjsonfile/<filename>")
+def ReadJsonFileRoute(filename):    
+    ''' Opens a JSON or GeoJSON file and then returns
+        its contents to the client. The filename is specified
+        as a parameter. '''
+
+    # Note that we have to assemble the complete filepath. We do this on the 
+    # server because the client has no knowledge of the server's file structure.
+    filepath = f"static/data/{filename}"
+
+    # Add some simple error handling to help if the user entered an invalid
+    # filename. 
+    try: 
+        with open(filepath) as f:    
+            json_data = json.load(f)
+    except:
+        json_data = {'Error': f'{filename} not found on server!'}
+
+    print('Returning data from a file')
+
+    return jsonify(json_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
