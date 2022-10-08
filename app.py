d1ea876197bc5f5d17c8093f101df8d5c1442b32
Kim Price <k_renick@yahoo.com>
2022-10-08 18:43:39 -0500

    Updated ETL, added extraction of data to index route in app.pyand have raw data feeding to index.html

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from telnetlib import theNULL
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 
 
 #Import json
@@ -23,10 +22,10 @@ base = automap_base()
 base.prepare(engine, reflect=True)
 
 #Reflect the tables
-crime_data = base.classes.crime_data
-demo_cat = base.classes.demo_cat
-demographic_data = base.classes.demographic_data
-neighborhood = base.classes.neighborhood
+crimeData = base.classes.crimeData
+demoCat = base.classes.demoCat
+demographicData = base.classes.demographicData
+neighborhoodData = base.classes.neighborhoodData
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -37,33 +36,69 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     print("this is my file")
-    return render_template('index.html')
+
+    # Open a session
+    session = Session(engine)
+
+    #Get list of neighborhoods with ID
+    neigh_results = session.query(neighborhoodData.neighborhoodID, neighborhoodData.neighborhood).filter(neighborhoodData.neighborhoodID == 1).all()
+    
+    #Get Demo data for all Minneapolis
+    sel = [demographicData.neighborhoodID, demoCat.demographic, demoCat.category, demographicData.percent]
+    demo_results = session.query(*sel).join(demographicData, demoCat.demoID == demographicData.demoID).filter(demographicData.neighborhoodID == 1).all()
+    
+    #Get Crime data for all Minneapolis
+    sel = [neighborhoodData.neighborhoodID, neighborhoodData.neighborhood, crimeData.occurred_date, crimeData.offense_cat, crimeData.offense, crimeData.latitude, crimeData.longitude, crimeData.crime_count]
+    crime_results = session.query(*sel).join(crimeData, neighborhoodData.neighborhoodID == crimeData.neighborhoodID).filter(neighborhoodData.neighborhoodID == 1).all()
+  
+    #Close the session
+    session.close()
+
+    #Put neighborhoods into a list of dictionaries
+    neighborhood_list = []
+    for neighborhoodData_neighborhoodID, neighborhoodData_neighborhood in neigh_results:
+        dict = {}
+        dict["neighborhoodID"] = neighborhoodData_neighborhoodID
+        dict["neighborhood"] = neighborhoodData_neighborhood
+        neighborhood_list.append(dict)
+    
+    #Put crime data into a list of dictionaries
+    crime_list = []
+    for record in crime_results:
+        (neighborhoodData_neighborhoodID, neighborhoodData_neighborhood, crimeData_occurred_date, crimeData_offense_cat, crimeData_offense, crimeData_latitude, crimeData_longitude, crimeData_crime_count) = record
+        dict = {}
+        dict["neighborhoodID"] = neighborhoodData_neighborhoodID
+        dict["neighborhood"] = neighborhoodData_neighborhood
+        dict["offense_cat"] = crimeData_offense_cat
+        dict["crime_count"] = crimeData_crime_count
+        dict["occured_date"] = crimeData_occurred_date
+        dict["offense"] = crimeData_offense
+        dict["latitude"] = crimeData_latitude
+        dict["longitude"] = crimeData_longitude
+        crime_list.append(dict)
+
+    #Put demographic data into a list of dictionaries
+    demo_list = []
+    for record in demo_results:
+        (demographicData_neighborhoodID, demoCat_demographic, demoCat_category, demographicData_percent) = record
+        dict = {}
+        dict["neighborhood_id"] = demographicData_neighborhoodID
+        dict["demographic"] = demoCat_demographic
+        dict["category"] = demoCat_category
+        dict["percent"] = demographicData_percent
+        demo_list.append(dict)
+
+    #Render the index.html and pass the lists containing neighborhood, demographic and crime data
+    return render_template('index.html', neighborhood_list=neighborhood_list, demo_list = demo_list, crime_list = crime_list)
 
 
 # Route to gather data from PostgreSQL DB: minne_crime_db
-@app.route("/getNeighborhoodData/<neighborhoodName>")
+@app.route("/getNeighborhoodData")
 def getSQL(neighborhoodName):
 
-    # Open a session, run the query to get crime data, and then close the session again
-    session = Session(engine)
-    if neighborhood = 'all':
-       results = session.query(neighborhood).join(crime_data, neighborhood.neighborhood_id==crime_data.neighborhood_id).all()
-    else:
-        results = session.query(neighborhood).join(crime_data, neighborhood.neighborhood_id==crime_data.neighborhood_id).filter(User.email == neighborhoodName).all()
 
-    session.close()
-
-    # Create a list of dictionaries, with each dictionary containing one row from the query. 
-    crime_info = []
-    for id, neighborhoodN, occurred_date in results:
-        dict = {}
-        dict["neighborhood_id"] = id
-        dict["neighborhoodN"] = neighborhoodN
-        dict["occurred_date"] = occurred_date
-        crime_info.append(dict)
-
-    # Redirect back to home page
-    return (crime_info)
+    # Neighborhood crime info
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
